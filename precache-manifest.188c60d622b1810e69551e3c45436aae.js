0cc65b7fbd390b8820d18c5d865879dae3614e42
Mstislav Zhivodkov <stevemyz@gmail.com>
2019-08-08 23:44:03 +0700

    Updates

---
+++ precache-manifest.188c60d622b1810e69551e3c45436aae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be50d54a6faddb89c3c0eae4f4f7b1b",
+    "url": "/teamlead-reigns/index.html"
+  },
+  {
+    "revision": "c0ddb89a7697b8e021eb",
+    "url": "/teamlead-reigns/static/css/main.df1081b0.chunk.css"
+  },
+  {
+    "revision": "06780b2c1946eb9b8af6",
+    "url": "/teamlead-reigns/static/js/2.5a235a96.chunk.js"
+  },
+  {
+    "revision": "c0ddb89a7697b8e021eb",
+    "url": "/teamlead-reigns/static/js/main.4f8c4e9f.chunk.js"
+  },
+  {
+    "revision": "06c2c6d46624db63f9ab",
+    "url": "/teamlead-reigns/static/js/runtime~main.11e54acd.js"
+  }
+]);
